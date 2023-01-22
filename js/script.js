3926349bcbbc5e7e8dee9e85e50bd4360de7ad4f
Luthfi Pratomo <lapratomo24@yahoo.com>
2023-01-22 22:27:29 +0100

    add initial variables

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guess = document.querySelector(".guess");
+const letter = document.querySelector(".letter");
+const wordProgress = document.querySelector(".word-in-progress");
+const remaining = document.querySelector(".remaining");
+const span= document.querySelector("span");
+const message = document.querySelector(".message");
+const hideButton = document.querySelector(".hide");
+const testWord = "magnolia";
+
