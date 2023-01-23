b505165c18c33ea5b01c12d6680380576f7b3861
Luthfi Pratomo <lapratomo24@yahoo.com>
2023-01-23 12:32:48 +0100

    add conditional blocks for letter input

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Initial variables
 const guessedLetters = document.querySelector(".guessed-letters");
 const guess = document.querySelector(".guess");
 const letter = document.querySelector(".letter");
@@ -8,6 +9,7 @@ const message = document.querySelector(".message");
 const hideButton = document.querySelector(".hide");
 const testWord = "magnolia";
 
+// Circles as placeholder
 const placeholder = (testWord) => {
   const testArray = [];
   for (let item of testWord) {
@@ -17,9 +19,29 @@ const placeholder = (testWord) => {
 };
 placeholder(testWord);
 
+// Form input check
 guess.addEventListener("click", (e) => {
   e.preventDefault();
+  message.innerText = "";
   const formInput = letter.value;
   console.log(formInput);
-  formInput.innerHTML = "";
+  letter.value = "";
+
+
+  const userInput = inputValue(formInput);
+  console.log(userInput);
 });
+
+// Validate player input
+const inputValue = (input) => {
+  const acceptedLetter = /[a-zA-Z]/;
+  if (input.length === 0) {
+    message.innerText = "Did you enter a letter?";
+  } else if (input.length > 1) {
+    message.innerText = "One letter at a time!";
+  } else if (!input.match(acceptedLetter)) {
+    message.innerText = "Enter a valid letter!";
+  } else {
+    return input;
+  }
+};
